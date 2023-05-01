bd7896340af5322107da15aa1775ea853e25088a
Sanjoth Rai <sanjr4279@gmail.com>
2023-05-01 19:33:29 +0530

    mapbox styles

--- src/Maps.js
+++ src/Maps.js
@@ -15,7 +15,11 @@ export default function Maps() {
         <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{width: "100%", height: "100%"}}>
             <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            url="https://api.mapbox.com/styles/v1/s-a-n-/ck9mgl30r3ieg1irzkqrizs96/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoicy1hLW4tIiwiYSI6ImNrOW1nOGQxdTA4MGszZWxxcHFjanZ1anEifQ.8tpL_-EP-4ZrmpLNllLYuQ"
+            // url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            //mapbox://styles/s-a-n-/ck9mgl30r3ieg1irzkqrizs96
+            // https://api.mapbox.com/styles/v1/s-a-n-/ck9mgl30r3ieg1irzkqrizs96/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoicy1hLW4tIiwiYSI6ImNrOW1nOGQxdTA4MGszZWxxcHFjanZ1anEifQ.8tpL_-EP-4ZrmpLNllLYuQ
+            // url="https://api.mapbox.com/styles/v1/s-a-n-/ck9mgl30r3ieg1irzkqrizs96.html?title=view&access_token=pk.eyJ1Ijoicy1hLW4tIiwiYSI6ImNrOW1nOGQxdTA4MGszZWxxcHFjanZ1anEifQ.8tpL_-EP-4ZrmpLNllLYuQ&zoomwheel=true&fresh=true#11.58/48.8631/2.3544"
             />
             <Marker position={position} icon={icon}>
             <Popup>
